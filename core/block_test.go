73bc6283ec35bfa657fd37e812451b3434f0692e
Kiran Pachhai <kiran.pachhai@gmail.com>
2023-12-22 15:22:11 -0500

    Created the blockchain data structure

--- core/block_test.go
+++ core/block_test.go
@@ -20,7 +20,14 @@ func randomBlock(height uint32) *Block {
 		Data: []byte("foo"),
 	}
 	return NewBlock(header, []Transaction{tx})
+}
 
+func randomBlockWithSignature(t *testing.T, height uint32) *Block {
+	privKey := crypto.GeneratePrivateKey()
+	b := randomBlock(height)
+	assert.Nil(t, b.Sign(privKey))
+
+	return b
 }
 
 func TestSignBlock(t *testing.T) {
